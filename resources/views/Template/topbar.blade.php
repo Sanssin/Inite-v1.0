ecf6e7e2d13b8574b3b92793a18ac631b5dc807a
Sanssin <nurihsanudin13@gmail.com>
2025-01-28 00:36:05 +0700

    modified:   resources/views/Template/topbar.blade.php 	modified:   resources/views/landing page/index.blade.php 	modified:   routes/web.php

--- resources/views/Template/topbar.blade.php
+++ resources/views/Template/topbar.blade.php
@@ -9,12 +9,12 @@
 
       <nav id="navmenu" class="navmenu">
         <ul>
-          <li><a href="#hero" class="active">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#portfolio">Nuclearpedia</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#pricing">Pricing</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#hero' : '#hero' }}" class="active">Home</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#about' : '#about' }}">About</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#portfolio' : '#portfolio' }}">Nuclearpedia</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#services' : '#services' }}">Services</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#team' : '#team' }}">Team</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#pricing' : '#pricing' }}">Pricing</a></li>
           <li class="dropdown"><a href="#"><span>Simulasi</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="#">Dropdown 1</a></li>
@@ -23,7 +23,7 @@
               <li><a href="#">Dropdown 4</a></li>
             </ul>
           </li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="{{ request()->is('portfolioDetail') ? url('/') . '#contact' : '#contact' }}">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortfolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing page/index');
 });
+
+Route::get('/portfolioDetail', [PortfolioController::class, 'showDetail']);

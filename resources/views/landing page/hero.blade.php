b57389d25b936b7d2a8f5721fbb64989f1f002aa
Sanssin <nurihsanudin13@gmail.com>
2025-01-27 17:14:36 +0700

    new file:   resources/views/landing page/about.blade.php 	new file:   resources/views/landing page/clients.blade.php 	new file:   resources/views/landing page/hero.blade.php 	new file:   resources/views/landing page/why-us.blade.php

---
+++ resources/views/landing page/hero.blade.php
@@ -0,0 +1,16 @@
+<section id="hero" class="hero section dark-background">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="zoom-out">
+        <h1>Selamat Datang! di Indonesian Nuclear Interactive Website</h1>
+        <p>Mari jelajahi teknologi nuklir di Indonesia</p>
+        <div class="d-flex">
+          <a href="#about" class="btn-get-started">Get Started</a>
+        </div>
+      </div>
+      <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="200">
+        <img src="{{ asset('template/assets/img/nuclear.png') }}" class="img-fluid animated" alt="" style="max-width: auto; height: 85%;" >
+      </div>
+    </div>
+  </div>
+</section>

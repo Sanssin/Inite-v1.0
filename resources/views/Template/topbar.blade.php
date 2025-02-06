dc6e84913e34cd9cccb7b2c21632a4e7616e9a67
Sanssin <nurihsanudin13@gmail.com>
2025-02-06 18:19:37 +0700

    modified:   public/template/assets/css/main.css 	modified:   resources/views/Template/topbar.blade.php

--- resources/views/Template/topbar.blade.php
+++ resources/views/Template/topbar.blade.php
@@ -22,20 +22,24 @@
       <a class="btn-getstarted" href="#about">Get Started</a>
     
       <ul class="navbar-nav">
-        <li class="nav-item dropdown profile-dropdown">
-          <a class="nav-link dropdown-toggle d-flex align-items-center" href="#"
-            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="{{ asset('template/assets/img/default.png') }}" alt="user" width="40" class="profile-pic rounded-circle object-fit-cover" />
-          </a>
-          <div class="dropdown-menu dropdown-menu-end mailbox dropdown-menu-animate-up text-center">
-            <div class="card-body profile-card">
-              <center class="mt-4">
-                <img src="{{ asset('template/assets/img/default.png') }}"/>
-                <h3 class="mt-2 text-dark">Guest</h3>
-              </center>
+      <li class="nav-item dropdown profile-dropdown">
+        <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="{{ asset('template/assets/img/default.png') }}" alt="user" width="40" class="profile-pic rounded-circle object-fit-cover" />
+        </a>
+        <div class="dropdown-menu dropdown-menu-end p-2" aria-labelledby="navbarDropdown">
+          <div class="card" style="width: 18rem;">
+            <img src="{{ asset('template/assets/img/default.png') }}" class="card-img-top rounded-circle mx-auto mt-3" alt="Profile Picture" style="width: 80px; height: 80px;">
+            <div class="card-body text-center">
+              <h3 class="card-title">Guest</h3>
+              <p class="card-text text-muted">user@example.com</p>
+              <hr>
+              <a href="#" class="btn btn-primary btn-sm w-100 mb-2">Profile</a>
+              <a href="#" class="btn btn-secondary btn-sm w-100 mb-2">Settings</a>
+              <a href="#" class="btn btn-danger btn-sm w-100">Logout</a>
             </div>
           </div>
-        </li>
-      </ul>
+        </div>
+      </li>
+    </ul>
     </div>
   </header>
